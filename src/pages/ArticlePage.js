f6f99d789272b8690790711f2e2d3770dcce753b
jayantsurana <jayantplb@gmail.com>
2019-10-03 18:59:21 +0530

    connected frontend with backend
    api call to db from frontend to show upvotes.

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
@@ -7,6 +7,18 @@ const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
 
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            console.log(body);
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name]);
+
     if(!article) return <NotFoundPage />
 
     const otherArticles = articleContent.filter(article => article.name !== name);
@@ -14,6 +26,7 @@ const ArticlePage = ({ match }) => {
     return (
         <>
         <h1>{article.title}</h1>
+        <p>This post has been upvoted {articleInfo.upvotes} times.</p>
         {article.content.map((paragraph, key) => (
             <p key={key}>{paragraph}</p>
         ))}
